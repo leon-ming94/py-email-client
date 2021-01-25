4ae24269cb4a00cc39b71a6db137062729d01240
denghaoming <denghaoming@supermonkey.com.cn>
2021-01-25 22:24:40 +0800

    新增邮件发送py文件

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+from setuptools import setup
+
+setup(
+    name='monkey-email_client',  # 包名字
+    version='1.1',  # 包版本
+    description='send email_client python',  # 简单描述
+    author='leon',  # 作者
+    author_email='denghaoming@supermonkey.com.cn',  # 作者邮箱
+    url='http://git.internal.supermonkey.com.cn:10080/supermonkey-data/monkey-py-email-manager.git',  # 包的主页
+    packages=['email_client'],  # 包
+    install_requires=[]
+)
