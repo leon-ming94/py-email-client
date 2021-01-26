1240471f74b1d69c151330be1dbcf6dbbfd8b660
denghaoming <denghaoming@supermonkey.com.cn>
2021-01-26 16:33:06 +0800

    修改setup.py文件

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup
 
 setup(
-    name='monkey-email_client',  # 包名字
-    version='1.1',  # 包版本
+    name='py-email-client',  # 包名字
+    version='1.0',  # 包版本
     description='send email_client python',  # 简单描述
-    author='leon',  # 作者
-    author_email='denghaoming@supermonkey.com.cn',  # 作者邮箱
-    url='http://git.internal.supermonkey.com.cn:10080/supermonkey-data/monkey-py-email-manager.git',  # 包的主页
+    author='leonming',  # 作者
+    author_email='13725911495@163.com',  # 作者邮箱
+    url='https://github.com/leon-ming94/py-email-client.git',  # 包的主页
     packages=['email_client'],  # 包
     install_requires=[]
 )
